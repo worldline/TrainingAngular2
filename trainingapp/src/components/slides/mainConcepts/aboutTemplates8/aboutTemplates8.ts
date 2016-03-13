1f6ba4cd3000604ee6be7544e7cd2fa9e1fc9d99
rlegrand <legrand.regis@gmail.com>
2016-03-13 19:40:14 +0100

    more about templates

---
+++ trainingapp/src/components/slides/mainConcepts/aboutTemplates8/aboutTemplates8.ts
@@ -0,0 +1,20 @@
+import {Component, View, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutTemplates8',
+	templateUrl:'src/components/slides/mainConcepts/aboutTemplates8/aboutTemplates8.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutTemplates8/aboutTemplates8.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutTemplates8 extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
