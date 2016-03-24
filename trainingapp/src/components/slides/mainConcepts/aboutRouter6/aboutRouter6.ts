09473c9b0b99f7dea9b42170b056b734580c4493
a571301 <regis.legrand@worldline.com>
2016-03-24 15:53:19 +0100

    Router ended (review todos)

---
+++ trainingapp/src/components/slides/mainConcepts/aboutRouter6/aboutRouter6.ts
@@ -0,0 +1,20 @@
+import {Component, View, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutRouter6',
+	templateUrl:'src/components/slides/mainConcepts/aboutRouter6/aboutRouter6.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutRouter6/aboutRouter6.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutRouter6 extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
---
+++ trainingapp/src/components/slides/mainConcepts/aboutRouter8/aboutRouter8.ts
@@ -0,0 +1,20 @@
+import {Component, View, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutRouter8',
+	templateUrl:'src/components/slides/mainConcepts/aboutRouter8/aboutRouter8.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutRouter8/aboutRouter8.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutRouter8 extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
