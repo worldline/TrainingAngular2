80d11f82878eef8ad7ac44fed33dde0b369fe3a2
rlegrand <legrand.regis@gmail.com>
2016-05-08 19:37:51 +0200

    More slides

---
+++ trainingapp/src/components/slides/mainConcepts/aboutLifecycle/aboutLifecycle.ts
@@ -0,0 +1,20 @@
+import {Component, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutLifecycle',
+	templateUrl:'src/components/slides/mainConcepts/aboutLifecycle/aboutLifecycle.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutLifecycle/aboutLifecycle.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutLifecycle extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
---
+++ trainingapp/src/components/slides/mainConcepts/aboutLifecycle4/aboutLifecycle4.ts
@@ -0,0 +1,20 @@
+import {Component, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutLifecycle4',
+	templateUrl:'src/components/slides/mainConcepts/aboutLifecycle4/aboutLifecycle4.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutLifecycle4/aboutLifecycle4.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutLifecycle4 extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
