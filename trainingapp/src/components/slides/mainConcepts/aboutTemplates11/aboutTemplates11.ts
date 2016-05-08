0a2171280d24038dfc8e0c3831a940e3e7f72f06
rlegrand <legrand.regis@gmail.com>
2016-05-08 12:29:45 +0200

    Fixes

---
+++ trainingapp/src/components/slides/mainConcepts/aboutTemplates11/aboutTemplates11.ts
@@ -0,0 +1,20 @@
+import {Component, Inject, ElementRef} from 'angular2/core';
+
+import {SlideCommon} from '../../slideCommon/slideCommon';
+import {Editor} from '../../../editor/editor';
+import {EditorTab} from '../../../editorTab/editorTab';
+
+import {HOST_SLIDE_CONTAINER_CLASS} from '../../../../services/constants';
+
+
+@Component({
+	selector:'AboutTemplates11',
+	templateUrl:'src/components/slides/mainConcepts/aboutTemplates11/aboutTemplates11.html',
+	styleUrls: ['src/components/slides/mainConcepts/aboutTemplates11/aboutTemplates11.css'],
+	directives: [Editor, EditorTab]
+})
+export class AboutTemplates11 extends SlideCommon{
+	constructor(elt: ElementRef, @Inject(HOST_SLIDE_CONTAINER_CLASS) hostClass: string) {
+		super(elt, hostClass);
+	}
+}
